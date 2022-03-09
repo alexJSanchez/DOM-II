d06e1aa72f3c97ec0dff140761eb248b70833e44
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-09 10:38:39 -0500

    select

--- src/index.js
+++ src/index.js
@@ -4,12 +4,39 @@ import './less/index.less'
 
 //mouseover
 document.querySelector(".logo-heading").addEventListener("mouseover", Event => {
-    Event.target.style.color = "pink"
+    Event.target.style.color = "red"
 });
 //keydown
 document.querySelector("body").addEventListener("keydown", Event => {
-    if (Event.target.style.backgroundColor === "black"){
+    if (Event.target.style.backgroundColor === "pink"){
         Event.target.style.backgroundColor = "white";
     }else{
-    Event.target.style.backgroundColor = "black";}
+    Event.target.style.backgroundColor = "pink";}
 });
+
+//wheel 
+document.querySelector(".intro p").addEventListener("wheel", Event => {
+    Event.target.style.fontSize = "10px";
+});
+
+//load
+window.addEventListener("load", Event => {
+   console.log("lets start coding!")
+})
+
+//focus
+document.querySelector("body").addEventListener("scroll", Event => {
+    document.querySelector("img-content").focus();
+})
+
+//resize
+window.addEventListener('resize', Event => {
+ console.log('please leave the size alone')
+})
+
+//scroll
+window.addEventListener('scroll', Event => {
+    console.log('scrolling on page')
+})
+
+
