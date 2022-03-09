599caef54c4998a34fd48d63d34b4589c3298629
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-08 19:50:02 -0500

    add mouseover

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//mouseover
+document.querySelector(".logo-heading").addEventListener("mouseover", Event => {
+    Event.target.style.color = "pink"
+});
+//keydown
+
+document.querySelector("body").addEventListener("keydown", Event => {
+    if (Event.target.style.backgroundColor === "black"){
+        Event.target.style.backgroundColor = "white";
+    }else{
+    Event.target.style.backgroundColor = "black";}
+});
